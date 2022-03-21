faea1f6d721ca8c8bb3aedb7ac3b7264a13cf2a4
RizqiZamzamijamil <rizqizamzamij@gmail.com>
2022-03-22 01:04:40 +0700

    Submit Jobsheet M5 Desain dan Pemrograman Web

---
+++ operator_4.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $x = 4;
+            $x += 3;
+            echo "hasil operasi x += 3 adalah = $x";
+            $x = 4;
+            $x -= 3;
+            echo "<br>hasil operasi x -= 3 adalah = $x";
+            $x = 4;
+            $x /= 3;
+            echo "<br>hasil operasi x /= 3 adalah = $x";
+            $x = 4;
+            $x %= 3;
+            echo "<br>hasil operasi x %= 3 adalah = $x";
+            $x = 4;
+            $x .= 3;
+            echo "<br>hasil operasi x .= 3 adalah = $x";
+        ?>
+    </body>
+</html>
